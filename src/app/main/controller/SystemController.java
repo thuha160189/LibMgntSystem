0ee8bfbd8e39c7df147f902f332f2d584ad2ac27
778899 <hailianzhang83@gmail.com>
2020-10-07 07:47:03 -0500

    test

--- src/app/main/controller/SystemController.java
+++ src/app/main/controller/SystemController.java
@@ -2,8 +2,17 @@ package app.main.controller;
 
 import app.main.dataaccess.DataAccess;
 import app.main.dataaccess.DataAccessFacade;
+import app.main.model.Book;
 
 public class SystemController {
 	private DataAccess dataaccess = new DataAccessFacade();
+	Book book;
+	
+
+	
+	 
+	 
+	 
+	
 
 }
