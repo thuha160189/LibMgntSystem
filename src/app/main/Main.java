ea77aa2e1716f1c39c5e3587afbd7e40fc0a017a
mohanlar96 <mohan.lar96@gmail.com>
2020-10-07 00:44:46 -0500

    login screen drawing complete

--- src/app/main/Main.java
+++ src/app/main/Main.java
@@ -1,26 +1,10 @@
 package app.main;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import app.main.ui.UI;
 
-public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
+public class Main  {
+
 	public static void main(String[] args) {
-		launch(args);
+		UI.main(args);
 	}
 }
